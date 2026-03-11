f239b16a191118a3bdf032739ad6987d680998a5
Alexandre <alexandrejespindola@gmail.com>
2026-03-10 21:22:39 -0300

    Inclusão de título

--- Diversos/IMC.py
+++ Diversos/IMC.py
@@ -1,3 +1,5 @@
+#Código para executar o cálculo do IMC e dar o resultado na tela
+
 nome = input("Digite o nome: ")
 peso = float(input("Informe o peso em Kg: "))
 altura = float(input("Informe a altura em metros (ex 1.75): ")) 
@@ -10,11 +12,11 @@ peso_normal = 18.5 <= imc < 25
 sobrepeso = 25 <= imc < 30
 obesidade = imc >= 30
 
-if baixo_peso:
+if baixo_peso:           
   print("Você está abaixo do peso.") 
 elif peso_normal:
     print("Você está com o peso normal.") 
 elif sobrepeso:
     print("Você está com sobrepeso.") 
 else:
-    print("Você está com obesidade.") 
+    print("Você está com obesidade.")
