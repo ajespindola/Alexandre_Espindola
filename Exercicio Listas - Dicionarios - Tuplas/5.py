c10971fc1ee5bb41400932f79c8cacb31ad696cc
Alexandre <alexandrejespindola@gmail.com>
2026-03-20 20:32:41 -0300

    EXEC

--- Exercicio Listas - Dicionarios - Tuplas/5.py
+++ Exercicio Listas - Dicionarios - Tuplas/5.py
@@ -5,20 +5,12 @@ fila2 = [nome1,nome2]
 fila.extend([nome1])
 fila.extend([nome2])
 print(fila)
-prior = int(input("Digite o prioritário: (1/2/3/4) "))
 
-if prior == 1:
-    fila[0] = "Ana"
-elif prior == 2:
-    fila[0] = "Bruno"
-    fila[1] = "Ana"
-elif prior == 3:
-    fila[0] = nome1
-    fila[2] = "Ana"
-elif prior == 4:
-    fila[0] = nome2
-    fila[3] = "Ana"
-else:
-    print("Número não encontrado!")
+prior = input("Digite o nome prioritário: ")
+fila.insert(0, prior)
 
 print(fila)
+
+ultimo = fila.pop(0)
+print(fila)
+print(f"O último nome da fila era: {ultimo}")
