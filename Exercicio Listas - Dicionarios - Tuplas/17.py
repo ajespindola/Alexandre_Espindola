bb06d546e23cf6f15f0a5976aeb8ea5873a98879
Alexandre <alexandrejespindola@gmail.com>
2026-03-20 21:30:45 -0300

    000

--- Exercicio Listas - Dicionarios - Tuplas/17.py
+++ Exercicio Listas - Dicionarios - Tuplas/17.py
@@ -6,4 +6,5 @@ f4 = int(input("Digite o quarto número: "))
 num = (f1, f2, f3, f4)
 
 print(f"A Tupla original é: {num}")
-print(f"A Tupla ordenada de forma crescente é: {sorted(num)} , {type(num)}")
+num2 = sorted(num)
+print(f"Tipo da variável ordenada é: {type(num2)}")
