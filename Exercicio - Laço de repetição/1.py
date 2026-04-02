bf073fbd98f4999a2133a11cca2dab91dfdf65ee
Alexandre <alexandrejespindola@gmail.com>
2026-04-01 22:55:57 -0300

    Laço

--- Exercicio - Laço de repetição/1.py
+++ Exercicio - Laço de repetição/1.py
@@ -1,35 +1,20 @@
 #Criar um sistema precisa registrar compras realizadas por clientes.
 
-#O programa deverá:
-#Solicitar o valor de uma compra
-#Somar os valores informados
-#Continuar pedindo valores até o usuário digitar 0
-
-#Ao final, mostrar:
-#Total das compras
-#Quantidade de compras realizadas
-#Valor médio das compras
-
-#Regras:
-#Utilize while
-#O valor 0 encerra o programa
-#Não pode usar for
-
-total_compras = 0
-quantidade_compras = 0
+total = 0
+quantidade = 0
 while True:
-    valor_compra = float(input("Digite o valor da compra (ou 0 para encerrar): "))
+    valor = float(input("Digite o valor da compra (ou 0 para encerrar): "))
     
-    if valor_compra == 0:
+    if valor == 0:
         break
     
-    total_compras += valor_compra
-    quantidade_compras += 1
-if quantidade_compras > 0:
-    valor_medio = total_compras / quantidade_compras
-    print(f"Total das compras: R${total_compras:.2f}")
-    print(f"Quantidade de compras realizadas: {quantidade_compras}")
-    print(f"Valor médio das compras: R${valor_medio:.2f}")
+    total += valor
+    quantidade += 1
+if quantidade > 0:
+    media = total / quantidade
+    print(f"Total das compras: R${total:.2f}")
+    print(f"Quantidade de compras realizadas: {quantidade}")
+    print(f"Valor médio das compras: R${media:.2f}")
 else:
     print("Nenhuma compra foi realizada.")
     
